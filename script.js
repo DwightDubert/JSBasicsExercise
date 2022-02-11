74d55eac56e5a1ad7793a803f4625794ab65505c
DwightDubert <dwight.dubert@stu.caverna.kyschools.us>
2022-02-11 14:13:58 -0600

    Star

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log(14);
+
+//numbers, strings, Booleans, Null, Undefined
+
+//operators; +, -, *, /, **, %
+
+console.log(1%5);
+console.log(6%5);
+
+console.log('I\'M MARY POPPINS Y\'ALL!' );
+
+console.log("Hawk" + "eye")
+
+console.log("Dwight".length + "Dubert".length);
+
+console.log("Avengers: Endgame"[16]);
